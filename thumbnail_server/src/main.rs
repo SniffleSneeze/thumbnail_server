6bd6ab216d7b8165ab40f2800eb4c1cbc89eb45a
Alexander Ferreira <Alex.Ferreira@phronesis.net>
2024-08-17 10:24:47 +0100

    add db connection

--- thumbnail_server/src/main.rs
+++ thumbnail_server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Ok;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Read the .env file and build environment variables
+    dotenv::dotenv()?;
+
+    // get envi database url string
+    let db_url = std::env::var("DATABASE_URL")?;
+
+    // create pool connection
+    let pool = sqlx::SqlitePool::connect(&db_url).await?;
+
+    // Run migrations
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    Ok(())
 }
